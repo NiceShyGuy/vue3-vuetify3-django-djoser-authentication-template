775bccc336cc910fb04140ff4340a73c3f1a720b
3V1LXD <nicholasshenson@gmail.com>
2022-01-03 19:41:45 -0600

    implement environ for env config file

--- backend/README.md
+++ backend/README.md
@@ -6,6 +6,13 @@
     python manage.py createsuperuser
 ```
 
+## Setup env
+
+```
+    rename .env.example .env
+    configure .env file
+```
+
 ## Run server
 
 ```
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+# Load environment variables from .env file
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t+f#cx$qg63(clhqwy0z)rz0slx%((6=55qxafsi01!sabo4v-'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
